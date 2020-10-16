6341e22c0fa8928aac5429e5b993b45a228ff781
pramithvidusara@gmail.com <43032202+TilangaPramith@users.noreply.github.com>
2020-10-16 13:16:52 +0530

    second commit

--- controllers/data.js
+++ controllers/data.js
@@ -1,13 +1,28 @@
+const UserAgent = require('user-agents');
 
 const viewData = async (req, res) => {
+    
     const { name, age, address, mobile } = req.body;
-    const ip = req.clientIp;
 
-    console.log(name, " ", age, " ", address, " ", mobile);
-    console.log(ip);
+    try{
+        const ip = req.clientIp;
 
-    const data = ip;
-    res.json({data});
+        const userAgent = new UserAgent(); 
+        console.log(userAgent.toString());
+    
+        const agent = userAgent.toString()
+    
+        console.log(name, " ", age, " ", address, " ", mobile);
+        console.log(ip);
+    
+        const data = ip;
+        res.json({data, agent});
+    } catch(err){
+        console.warn(`Generic: ${err}`);
+        res.status(500).send();
+    }
+
+   
 }
 
 module.exports = { viewData }
