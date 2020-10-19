1b071d49d077ab35ec9443e4f3d24d0fa494b9d0
pramithvidusara@gmail.com <43032202+TilangaPramith@users.noreply.github.com>
2020-10-19 11:14:55 +0530

    Json Testing

--- controllers/data.js
+++ controllers/data.js
@@ -1,38 +1,48 @@
 const UserAgent = require('user-agents');
 
 const viewData = async (req, res) => {
-  if (req.body) {
-    this.name = req.body.name;
-    this.age = req.body.age;
-    this.address = req.body.address;
-    this.mobile = req.body.mobile;
-  }
-  if (req.query) {
-    this.name = req.query.name;
-    this.age = req.query.age;
-    this.address = req.query.address;
-    this.mobile = req.query.mobile;
-  }
   try {
-    const ip = req.clientIp;
+    if (JSON.parse(JSON.stringify(req.query)) || JSON.parse(JSON.stringify(req.body))) {
+      if (req.body) {
+        this.name = req.body.name;
+        this.age = req.body.age;
+        this.address = req.body.address;
+        this.mobile = req.body.mobile;
+      }
+      if (req.query) {
+        this.name = req.query.name;
+        this.age = req.query.age;
+        this.address = req.query.address;
+        this.mobile = req.query.mobile;
+      }
+      try {
+        const ip = req.clientIp;
 
-    const userAgent = new UserAgent();
-    // eslint-disable-next-line no-console
-    console.log(userAgent.toString());
+        const userAgent = new UserAgent();
+        // eslint-disable-next-line no-console
+        console.log(userAgent.toString());
 
-    const agent = userAgent.toString();
+        const agent = userAgent.toString();
 
-    // eslint-disable-next-line no-console
-    console.log(this.name, ' ', this.age, ' ', this.address, ' ', this.mobile);
-    // eslint-disable-next-line no-console
-    console.log(ip);
+        // eslint-disable-next-line no-console
+        console.log(this.name, ' ', this.age, ' ', this.address, ' ', this.mobile);
+        // eslint-disable-next-line no-console
+        console.log(ip);
 
-    const arr = [{ ip }, { agent }];
-    res.json(arr);
+        const arr = [{ ip }, { agent }];
+        res.json(arr);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.warn(`Generic: ${err}`);
+        res.status(500).send();
+      }
+    } else {
+      res.status(400).send({ message: 'SyntaxError: Unexpected token in JSON body' });
+    }
   } catch (err) {
     // eslint-disable-next-line no-console
     console.warn(`Generic: ${err}`);
-    res.status(500).send();
+    res.status(400).send({ message: 'SyntaxError: Unexpected token in JSON body' });
   }
 };
 
--- test/appTest.js
+++ test/appTest.js
@@ -14,10 +14,21 @@ describe('Task Api', () => {
     it('it should be GET correct data', (done) => {
       chai.request(app)
         .get('/data/getData')
+        .type('json')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('array');
           res.body.length.should.be.equal(2);
+          done();
+        });
+    });
+    // eslint-disable-next-line no-undef
+    it('invalid JSON', (done) => {
+      chai.request(app)
+        .get('/data/getData')
+        .type('invalid json')
+        .end((err, res) => {
+          console.log(res.error);
           done();
         });
     });
@@ -29,10 +40,21 @@ describe('Task Api', () => {
     it('it should be GET correct data', (done) => {
       chai.request(app)
         .post('/data/getData')
+        .type('json')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('array');
           res.body.length.should.be.equal(2);
+          done();
+        });
+    });
+    // eslint-disable-next-line no-undef
+    it('invalid JSON', (done) => {
+      chai.request(app)
+        .post('/data/getData')
+        .type('invalid json')
+        .end((err, res) => {
+          console.log(res.error);
           done();
         });
     });
